f475f14c3f8826e3de8d6e0b04a7f450069c4fe7
Vincenzo Romano <enzxx84@gmail.com>
2018-07-16 22:27:39 +0200

    reset intent

--- src/classes/input/intent-class.php
+++ src/classes/input/intent-class.php
@@ -67,8 +67,7 @@ class Intent {
 			$object_vars = get_object_vars(  $this->object->slots );
 
 			foreach( $object_vars AS $object_var ) {
-				$this->slots[ $object_var->name ] = json_encode($object_var, JSON_PRETTY_PRINT);
-
+				$this->slots[ $object_var->name ] = $object_var->value;
 			}
 		}
 
